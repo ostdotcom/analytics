ba7ed9adeec2dccc43310dab7a5d6948f5f25810
Tejas-Sangani <tejas@ost.com>
2019-03-08 19:32:09 +0530

    migrations for redshift tables.

--- migrations/create_pentaho.sql
+++ migrations/create_pentaho.sql
@@ -56,7 +56,7 @@ drop table if exists "dim_time" CASCADE;
     "token_sk" bigint  NOT NULL ,
     "meta_type_sk" bigint  NOT NULL ,
     "meta_name_sk" bigint  NOT NULL ,
-    "status" varchar(255) NOT NULL,
+    "status" varchar(20) NOT NULL,
     "total_transactions" bigint NOT NULL,
     "total_transfers" bigint NOT NULL,
     "total_volume" bigint NOT NULL,
@@ -67,3 +67,63 @@ drop table if exists "dim_time" CASCADE;
   ALTER TABLE token_transfer_facts ADD FOREIGN KEY (token_sk) REFERENCES dim_tokens (token_sk);
   ALTER TABLE token_transfer_facts ADD FOREIGN KEY (meta_type_sk) REFERENCES dim_meta_type (meta_type_sk);
   ALTER TABLE token_transfer_facts ADD FOREIGN KEY (meta_name_sk) REFERENCES dim_meta_name (meta_name_sk);
+
+DROP TABLE IF EXISTS transactions;
+CREATE TABLE transactions
+(
+  transaction_uuid    VARCHAR(255) NOT NULL,
+  transaction_hash    VARCHAR(255) NOT NULL,
+  gas_used            BIGINT NOT NULL,
+  status              VARCHAR(20) NOT NULL,
+  status_internal     VARCHAR(20) NOT NULL,
+  block_number        BIGINT NOT NULL,
+  value               BIGINT NOT NULL,
+  client_id           BIGINT NOT NULL,
+  token_id            BIGINT NOT NULL,
+  kind                INT NOT NULL
+)
+DISTKEY (client_id) SORTKEY (block_number);
+
+DROP TABLE IF EXISTS transactions;
+CREATE TABLE transactions
+(
+  transaction_uuid    VARCHAR(255) NOT NULL,
+  transaction_hash    VARCHAR(255) NOT NULL,
+  gas_used            BIGINT NOT NULL,
+  status              VARCHAR(20) NOT NULL,
+  status_internal     VARCHAR(20) NOT NULL,
+  block_number        BIGINT NOT NULL,
+  value               BIGINT NOT NULL,
+  client_id           BIGINT NOT NULL,
+  token_id            BIGINT NOT NULL,
+  kind                INT NOT NULL
+)
+DISTKEY (client_id) SORTKEY (block_number);
+
+DROP TABLE IF EXISTS tokens;
+CREATE TABLE tokens
+(
+  token_id            BIGINT NOT NULL,
+  symbol              VARCHAR(255) NOT NULL,
+  conversion_factor   decimal(15,6) NOT NULL,
+  decimal             int NOT NULL
+)
+
+DROP TABLE IF EXISTS transfers;
+CREATE TABLE transfers
+(
+  transaction_hash    VARCHAR(255) NOT NULL,
+  event_index         INT NOT NULL,
+  block_number        BIGINT NOT NULL,
+  contract_address    VARCHAR(255) NOT NULL,
+  amount              BIGINT NOT NULL
+)
+
+DROP TABLE IF EXISTS transaction_meta;
+CREATE TABLE transaction_meta
+(
+  transaction_meta_id    BIGINT NOT NULL,
+  transaction_uuid       VARCHAR(255) NOT NULL,
+  token_id               BIGINT NOT NULL,
+  kind                   INT NOT NULL
+)
