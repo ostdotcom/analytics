84a5cad7d1241f0c4e73640088010f372bfde586
bala <bala@ost.com>
2019-04-03 19:00:13 +0530

    script locking

--- executables/extract_and_transform.sh
+++ executables/extract_and_transform.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
+LOCKFILE="/tmp/`basename $0`"
+LOCKFD=200
+
+# PRIVATE
+_lock()             { flock -$1 $LOCKFD; }
+_no_more_locking()  { _lock u; _lock xn && rm -f $LOCKFILE; }
+_prepare_locking()  { eval "exec $LOCKFD>\"$LOCKFILE\""; trap _no_more_locking EXIT; }
+
+# ON START
+_prepare_locking
+
+# PUBLIC
+exlock_now()        { _lock xn; }  # obtain an exclusive lock immediately or fail
+exlock()            { _lock x; }   # obtain an exclusive lock
+shlock()            { _lock s; }   # obtain a shared lock
+unlock()            { _lock u; }   # drop a lock
+
+### BEGIN OF SCRIPT ###
+
+# Simplest example is avoiding running multiple instances of script.
+exlock_now || exit 1;
+
 while [[ $# -gt 0 ]]
 do
     key="$1";
