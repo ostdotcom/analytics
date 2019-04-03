ce3cbda8215c45b376844422a7362ca02e6cbf0b
bala <bala@ost.com>
2019-04-03 17:02:18 +0530

    wrapper executable fixes

--- executables/extract_and_transform.sh
+++ executables/extract_and_transform.sh
@@ -4,7 +4,7 @@ while [[ $# -gt 0 ]]
 do
     key="$1";
 
-    usage_str="Usage: ./extract_and_transform.sh --chain-id <Number> --start-block-no <Number> --end-block-no <Number>";
+    usage_str="Usage: ./extract_and_transform.sh --chain-id <Number> --start-block-no <Number>";
     # Read parameters
     case $key in
         --chain-id)
@@ -17,11 +17,6 @@ do
             shift # past argument
             shift # past value
             ;;
-        --end-block-no)
-            END_BLOCK_NO="$2"
-            shift # past argument
-            shift # past value
-            ;;
         *)    # unknown option
             echo $usage_str;
             exit 1
@@ -31,10 +26,18 @@ done
 
 # Extract data
 printf "\n\n Extraction started"
-/bin/node executables/extract_data.js --blockScanner true --token true --chainId ${CHAIN_ID} --startBlock ${START_BLOCK_NO} --endBlock ${END_BLOCK_NO}
+/bin/node executables/extract_data.js --blockScanner true --token true --chainId ${CHAIN_ID} --startBlock ${START_BLOCK_NO}
+if [[ $? != 0 ]]; then
+    echo "Error in Extraction!";
+    exit 1;
+fi
 printf "Extraction ended \n\n"
 
 # Transform data
 printf "\n\n Transformation started"
 /bin/bash executables/transform_data.sh --chain-id ${CHAIN_ID}
+if [[ $? != 0 ]]; then
+    echo "Error in Transformation!";
+    exit 1;
+fi
 printf "Transformation ended \n\n"
