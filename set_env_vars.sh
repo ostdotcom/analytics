9b3b9f87423994f0749f02d8c35fefbc04778e37
Mayur Patil <mayurupatil@gmail.com>
2019-04-15 01:24:07 +0530

    revert commit

--- set_env_vars.sh
+++ set_env_vars.sh
@@ -1,33 +1,34 @@
-
-export SUB_ENVIRONMENT='main'
 export ENVIRONMENT='development'
-export ENV_SUFFIX='_d7'
+export SUB_ENVIRONMENT='main'
+export ENV_SUFFIX='_d6'
 export SAAS_MYSQL_DATABASE_ENVIRONMENT='staging'
 
 export KIT_MYSQL_CONNECTION_POOL_SIZE='3'
 export KIT_SAAS_SUBENV_MYSQL_HOST='127.0.0.1'
-export KIT_SAAS_SUBENV_MYSQL_USER='analytics_read'
-export KIT_SAAS_SUBENV_MYSQL_PASSWORD='f234jhfruir12s*sdfyuqws'
+export KIT_SAAS_SUBENV_MYSQL_USER='saas_read'
+export KIT_SAAS_SUBENV_MYSQL_PASSWORD='sdfh732gFdHK7936aIghLjortab34793'
 export KIT_SAAS_SUBENV_MYSQL_PORT=3307
 
-export PRESTAGING_REDSHIFT_USER='ost_pentaho_stag_user'
-export PRESTAGING_REDSHIFT_DATABASE='analytics_staging'
-export PRESTAGING_REDSHIFT_PASSWORD='oSt2018Stag!Pentaho'
+export PRESTAGING_REDSHIFT_USER='analytics_user'
+export PRESTAGING_REDSHIFT_DATABASE='analytics'
+export PRESTAGING_REDSHIFT_PASSWORD='gdhsD56Sw248jljt68dfTRaxMn'
 export PRESTAGING_REDSHIFT_PORT='5439'
-export PRESTAGING_REDSHIFT_HOST='pepo.cfwzkfyo2mfi.eu-west-1.redshift.amazonaws.com'
+export PRESTAGING_REDSHIFT_HOST='redshift-instance.cl2x2qyuekxx.us-east-1.redshift.amazonaws.com'
 export PRESTAGING_SCHEMA_PREFIX='ost_warehouse_'
+
+
 
 export TRANSFERS_BATCH_SIZE=80
 export TRANSACTION_BATCH_SIZE=60
 export MAX_SPLIT_COUNT=5
-export NO_OF_BLOCKS_TO_PROCESS_TOGETHER=1
-export S3_WRITE_COUNT=2
+export NO_OF_BLOCKS_TO_PROCESS_TOGETHER=5
+export S3_WRITE_COUNT=100
 
-export S3_IAM_ROLE='arn:aws:iam::274208178423:role/ost_redshift_copy'
+export S3_IAM_ROLE='arn:aws:iam::704700004548:role/redshift_s3_copy'
 export S3_REGION='us-east-1'
-export S3_ACCESS_KEY='AKIAJ4KB25BTIAYS6YRA'
-export S3_ACCESS_SECRET='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'
-export S3_BUCKET_NAME='temp-analytics.ost.com'
+export S3_ACCESS_KEY='AKIA2IE3EXDCONIPVQEL'
+export S3_ACCESS_SECRET='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'
+export S3_BUCKET_NAME='analytics.stagingost.com'
 
 export LOCAL_DIR_FILE_PATH='/tmp/analytics_prestaging_data'
-export BLOCK_SCANNER_CONFIG_FILE_PATH='/Users/mayurpatil/workspace/analytics/block_scanner_config.json'
+export BLOCK_SCANNER_CONFIG_FILE_PATH=''
