9c09888e5ecd9ac56509fd73c512e4ea932e3e7e
Tejas-Sangani <tejas@ost.com>
2019-05-17 15:25:36 +0530

    mdx replace aux_chain_id and db_schema_prefix changes.

--- renameMdxFiles.js
+++ renameMdxFiles.js
@@ -7,6 +7,7 @@ const rootPrefix = ".",
 
 
 // commander
+//executable: node renameMdxFiles.js --auxChainId 202 --dBSchemaPrefix main_d6
 program
     .version('0.1.0')
     .option('--auxChainId <auxChainId>', 'aux Chain Id, mandatory param')
@@ -25,18 +26,21 @@ class RenameMdxFiles {
     }
 
     renameMysqlSchemaXml(){
+        var auxChainIds = program.auxChainId;
+        var auxChainIdsArray = auxChainIds.split(",");
         nunjucks.configure('pdi/mdx', { autoescape: true });
-        var fileText = nunjucks.render('mysql_schema.xml', { DB_SCHEMA_PREFIX: program.dBSchemaPrefix, AUX_CHAIN_1: program.auxChainId });
-        var fileName = "mysql_schema_" +  program.dBSchemaPrefix + "_" + program.auxChainId;
-        var filePath = Constant.TEMPORARY_MDX_DIRECTORY_FILE_PATH + fileName;
+        for(var i=0; i<auxChainIdsArray.length; i++){
+            var fileText = nunjucks.render('mysql_schema.xml', { DB_SCHEMA_PREFIX: program.dBSchemaPrefix, AUX_CHAIN_1: auxChainIdsArray[i] });
+            var fileName = "mysql_schema_" +  program.dBSchemaPrefix + "_" + auxChainIdsArray[i];
+            var filePath = Constant.TEMPORARY_MDX_DIRECTORY_FILE_PATH + fileName;
+            fs.writeFile(filePath, fileText, function(err) {
+                if(err) {
+                    return console.log(err);
+                }
 
-        fs.writeFile(filePath, fileText, function(err) {
-            if(err) {
-                return console.log(err);
-            }
-
-            logger.log("The file was saved!");
-        });
+                logger.log("The file was saved!");
+            });
+        }
     }
 
     renameRedshiftSchemaXml(){
