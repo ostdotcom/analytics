6357d776a43cdf6f800bf777d8bf5fd968c3ce2e
aman-barbaria <amanbarbaria@gmail.com>
2019-06-26 14:34:25 +0530

    max creation time for rds instance increased

--- services/check_rds_instance.js
+++ services/check_rds_instance.js
@@ -114,8 +114,8 @@ class CheckRDSInstance {
      */
     async checkAvailabilityOfRDSInstance(params) {
         const oThis = this;
-        const maxTimeInMinsToWait = 60, // wait for that much time to change status of instance to available before sending error
-            warningTimeInMinsToWait = 30; // wait for that much time to send warning mail
+        const maxTimeInMinsToWait = 180, // wait for that much time to change status of instance to available before sending error
+            warningTimeInMinsToWait = 60; // wait for that much time to send warning mail
         let creationTime = params.creationTime;
         let warnEmailSent = false;
         let timeStep = 0;
